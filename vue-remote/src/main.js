61474f926738c85dcd19238e0f29d1d1a2c2c863
rush <rush@wusaby.com>
2023-05-14 12:13:01 +0300

    Add primevue

--- vue-remote/src/main.js
+++ vue-remote/src/main.js
@@ -1,5 +1,10 @@
+import "primevue/resources/themes/lara-light-indigo/theme.css";  
 import { createApp } from 'vue'
+import PrimeVue from 'primevue/config'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+app.use(PrimeVue)
+app.use(router)
+app.mount('#app')
